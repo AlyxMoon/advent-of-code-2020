0a6bdd5f515cd1ff61faa490c13fd1d3c6cbcc62
AlyxMoon <allisterkmoon@gmail.com>
2020-12-15 12:43:34 -0800

    day 1 part 2

--- days/day1.js
+++ days/day1.js
@@ -1,25 +1,47 @@
 const getInput = require('../lib/getInput')
 
 const handleInput = async () => {
-  const rawData = await getInput(1)
-
-  return new Map(
-    rawData.trim()
-      .split('\n')
-      .map(line => [Number(line), true]),
-  )
+  return (await getInput(1)).trim().split('\n')
 }
 
-const main = async () => {
+const part1 = async () => {
   const sumVal = 2020
-  const numMap = await handleInput()
+  const numMap = new Map(
+    (await handleInput()).map(line => [Number(line), null]),
+  )
 
   for (const num of numMap.keys()) {
     if (numMap.has(sumVal - num)) {
-      console.log(num * (sumVal - num))
+      console.log(`Part 1: ${num * (sumVal - num)}`)
       break
     }
   }
+}
+
+const part2 = async () => {
+  const sumVal = 2020
+  const nums = (await handleInput()).map(Number)
+
+  for (let x = 0; x < nums.length - 2; x++) {
+    for (let y = x + 1; y < nums.length - 1; y++) {
+      const [val1, val2] = [nums[x], nums[y]]
+
+      if (val1 + val2 >= sumVal) continue
+      for (let z = y + 1; z < nums.length; z++) {
+        const val3 = nums[z]
+
+        if (val1 + val2 + val3 === sumVal) {
+          console.log(`Part 2: ${val1 * val2 * val3}`)
+          break
+        }
+      }
+    }
+  }
+}
+
+const main = async () => {
+  await part1()
+  await part2()
 }
 
 main()
