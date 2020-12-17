37dbae38654f8c16e6e4b77b8fe6c96262a62ab9
AlyxMoon <allisterkmoon@gmail.com>
2020-12-16 16:06:47 -0800

    day 9 part 2

--- src/solutions/day9.js
+++ src/solutions/day9.js
@@ -30,6 +30,50 @@ const part1 = (rawInput = '', preambleLength = 25) => {
 }
 
 const part2 = (rawInput = '', preambleLength = 25) => {
+  const numberCypher = handleInput(rawInput)
+  let invalidNum
+
+  for (let i = preambleLength; i < numberCypher.length; i++) {
+    const [num, prev] = [
+      numberCypher[i],
+      numberCypher.slice(i - preambleLength, i),
+    ]
+
+    let isValid = false
+    for (let i = 0; i < prev.length - 1; i++) {
+      for (let j = 1; j < prev.length; j++) {
+        if (num === prev[i] + prev[j]) {
+          isValid = true
+          break
+        }
+      }
+
+      if (isValid) break
+    }
+
+    if (!isValid) invalidNum = num
+  }
+
+  const numRange = [0, 1]
+  let currentVal = numberCypher[0] + numberCypher[1]
+
+  while (numRange[0] < numberCypher.length - 1) {
+    if (currentVal === invalidNum) {
+      const min = Math.min(...numberCypher.slice(numRange[0], numRange[1] + 1))
+      const max = Math.max(...numberCypher.slice(numRange[0], numRange[1] + 1))
+      return min + max
+    }
+
+    if (currentVal > invalidNum) {
+      numRange[0]++
+      numRange[1] = numRange[0] + 1
+      currentVal = numberCypher[numRange[0]] + numberCypher[numRange[1]]
+      continue
+    }
+
+    numRange[1]++
+    currentVal += numberCypher[numRange[1]]
+  }
 }
 
 module.exports = {
--- test/unit/spec/day9.spec.js
+++ test/unit/spec/day9.spec.js
@@ -35,15 +35,35 @@ describe('Day 9', () => {
     expect(solutions.part1(data)).toEqual(1930745883)
   })
 
-  it.skip('part 2 passes example dataset', () => {
+  it('part 2 passes example dataset', () => {
     const testData = `
+      35
+      20
+      15
+      25
+      47
+      40
+      62
+      55
+      65
+      95
+      102
+      117
+      150
+      182
+      127
+      219
+      299
+      277
+      309
+      576
     `
 
-    expect(solutions.part2(testData)).toEqual()
+    expect(solutions.part2(testData, 5)).toEqual(62)
   })
 
-  it.skip('part 2 gives correct solution', async () => {
+  it('part 2 gives correct solution', async () => {
     const data = await getInput(9)
-    expect(solutions.part2(data)).toEqual()
+    expect(solutions.part2(data)).toEqual(268878261)
   })
 })
